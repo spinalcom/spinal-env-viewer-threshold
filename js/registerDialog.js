72a08a0551a36f41cd050d6ec7b23340e2e12288
mbaradji221 <mbaradji221@outlook.fr>
2019-05-09 18:06:44 +0200

    add alarm panel

--- js/registerDialog.js
+++ js/registerDialog.js
@@ -1,10 +1,17 @@
 import vue from "vue";
 import thresholdConfigDialog from "../vue/thresholdConfig.vue";
 import thresholdGlobalConfig from "../vue/thresholdGlobalConfig.vue"
+import AlarmPanel from "../vue/alarmPanel.vue";
 
 const {
   SpinalMountExtention
 } = require("spinal-env-viewer-panel-manager-service");
+
+const {
+  SpinalForgeExtention
+} = require("spinal-env-viewer-panel-manager-service_spinalforgeextention");
+
+
 
 const dialogs = [{
     name: "thresholdConfigDialog",
@@ -22,3 +29,25 @@ const dialogs = [{
 for (let index = 0; index < dialogs.length; index++) {
   SpinalMountExtention.mount(dialogs[index]);
 }
+
+
+
+/***/ ////////////////// Register Panel /////////////////////////////// */
+
+const alarmExtension = SpinalForgeExtention.createExtention({
+  name: "spinal_alarm_panel",
+  vueMountComponent: vue.extend(AlarmPanel),
+  panel: {
+    title: "Alarm",
+    classname: "spinal_alarm_panel",
+    closeBehaviour: "hide"
+  },
+  style: {
+    "min-width": "460px",
+    "overflow": "hidden",
+    left: "400px"
+  }
+});
+
+SpinalForgeExtention.registerExtention("spinal_alarm_panel",
+  alarmExtension);
---
+++ vue/alarmPanel.vue
@@ -0,0 +1,145 @@
+
+<template>
+  <md-content class="alarmPanelContent">
+    <div class="_title">{{title}}</div>
+
+    <div class="tableContainer">
+      <md-table class="tableMd"
+                v-model="alarms"
+                md-sort="alarm.beginDate"
+                md-fixed-header
+                md-sort-order="asc">
+        <!-- <md-table-toolbar>
+          <h1 class="md-title">Alarms</h1>
+        </md-table-toolbar> -->
+
+        <md-table-empty-state md-label="No Alarm"
+                              :md-description="`This Element has no Alarm`">
+        </md-table-empty-state>
+
+        <md-table-row slot="md-table-row"
+                      slot-scope="{ item }">
+          <md-table-cell md-label="Name"
+                         md-sort-by="name"
+                         md-numeric>{{ item.name }}</md-table-cell>
+          <md-table-cell md-label="Begin"
+                         md-sort-by="alarm.beginDate">{{ item.alarm.beginDate |
+            formatDate
+            }}</md-table-cell>
+          <md-table-cell md-label="End"
+                         md-sort-by="alarm.endDate">{{ item.alarm.endDate |
+            formatDate }}</md-table-cell>
+          <md-table-cell md-label="type"
+                         md-sort-by="alarm.alarmType">{{ item.alarm.alarmType
+            }}</md-table-cell>
+
+        </md-table-row>
+      </md-table>
+    </div>
+
+  </md-content>
+</template>
+
+<script>
+const {
+  spinalPanelManagerService
+} = require("spinal-env-viewer-panel-manager-service");
+
+import { SpinalGraphService } from "spinal-env-viewer-graph-service";
+
+import { alarmService } from "spinal-organ-threshold";
+
+export default {
+  name: "alarm-panel",
+  data() {
+    return {
+      endpointNode: null,
+      title: null,
+      alarms: []
+    };
+  },
+  methods: {
+    opened(option) {
+      this.setTitle(option.name.get());
+      this.title = option.name.get();
+      this.endpointNode = option;
+      this.getAlarm();
+    },
+    setTitle(title) {
+      spinalPanelManagerService.panels.spinal_alarm_panel.panel.setTitle(
+        `Alarm : ${title}`
+      );
+    },
+    getAlarm() {
+      SpinalGraphService.getChildren(this.endpointNode.id.get(), [
+        alarmService.ENDPOINT_TO_ALARM_RELATION
+      ]).then(alarms => {
+        // this.alarms = [];
+
+        alarms = alarms.map(async el => {
+          return {
+            name: el.name.get(),
+            alarm: await el.element.load()
+          };
+        });
+
+        Promise.all(alarms).then(res => {
+          this.alarms = res.map(el => {
+            return {
+              name: el.name,
+              alarm: el.alarm.get()
+            };
+          });
+        });
+      });
+    }
+  },
+  filters: {
+    formatDate: function(value) {
+      let date = new Date(value);
+
+      let day = date.getDate();
+      let month = date.getMonth();
+      let year = date.getFullYear();
+      let hour = date.getHours();
+      let minute = date.getMinutes();
+      let second = date.getSeconds();
+
+      return `${day}/${month}/${year} ${hour}:${minute}:${second}`;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.alarmPanelContent {
+  width: 100%;
+  height: 100%;
+}
+
+._title {
+  height: 5%;
+  text-align: center;
+  font-size: 18px;
+  padding-top: 8px;
+  text-transform: uppercase;
+}
+
+.tableContainer {
+  margin-top: 10px;
+  height: 85%;
+  overflow: hidden;
+}
+
+.tableContainer .tableMd {
+  /* width: 100%; */
+  height: 100%;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+
+.tableContainer .tableMd .md-table-fixed-header {
+  height: 60px !important;
+  background: red;
+}
+</style>
